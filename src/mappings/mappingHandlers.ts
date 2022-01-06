651c1f9881b8508d71a220b7585a2af58f70398f
JQQQ <jiqiang90@hotmail.com>
2022-01-06 15:35:27 +1300

    use bulkCreate method

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,42 +1,42 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {SpecVersion,Event,Extrinsic} from "../types";
+import {EventRecord } from "@polkadot/types/interfaces"
+import {SubstrateExtrinsic,SubstrateBlock} from "@subql/types";
+import { SpecVersion, Event, Extrinsic} from "../types";
+import { wrapExtrinsics } from "../utils";
 
-
+let specVersion: SpecVersion;
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    const specVersion = await SpecVersion.get(block.specVersion.toString());
-    if(specVersion === undefined){
-        const newSpecVersion = new SpecVersion(block.specVersion.toString());
-        newSpecVersion.blockHeight = block.block.header.number.toBigInt();
-        await newSpecVersion.save();
+    if (!specVersion) {
+        specVersion = await SpecVersion.get(block.specVersion.toString());
     }
+
+    if(!specVersion || specVersion.id !== block.specVersion.toString()){
+        specVersion = new SpecVersion(block.specVersion.toString());
+        specVersion.blockHeight = block.block.header.number.toBigInt();
+        await specVersion.save();
+    }
+    const events = block.events.filter(evt => evt.event.section!=='system' && evt.event.method!=='ExtrinsicSuccess').map((evt, idx)=>handleEvent(block.block.header.number.toString(), idx, evt));
+    const calls = wrapExtrinsics(block).map((ext,idx)=>handleCall(`${block.block.header.number.toString()}-${idx}`,ext));
+    await Promise.all([
+        store.bulkCreate('Event', events),
+        store.bulkCreate('Extrinsic', calls)
+    ]);
 }
 
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    //skip system.ExtrinsicSuccess event
-    if(event.event.section !== 'system' && event.event.method !== 'ExtrinsicSuccess'){
-        const thisEvent = await Event.get(`${event.block.block.header.number}-${event.idx.toString()}`);
-        if(thisEvent === undefined){
-            const newEvent = new Event(`${event.block.block.header.number}-${event.idx.toString()}`);
-            newEvent.blockHeight = event.block.block.header.number.toBigInt();
-            newEvent.module = event.event.section;
-            newEvent.event = event.event.method;
-            await newEvent.save();
-        }
-    }
+export function handleEvent(blockNumber: string, eventIdx: number, event: EventRecord): Event {
+    const newEvent = new Event(`${blockNumber}-${eventIdx}`);
+    newEvent.blockHeight = BigInt(blockNumber);
+    newEvent.module = event.event.section;
+    newEvent.event = event.event.method;
+    return newEvent;
 }
 
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const thisExtrinsic = await Extrinsic.get(`${extrinsic.block.block.header.number}-${extrinsic.idx}`);
-    if(thisExtrinsic === undefined){
-        const newExtrinsic = new Extrinsic(extrinsic.extrinsic.hash.toString());
-        newExtrinsic.txHash = extrinsic.extrinsic.hash.toString();
-        newExtrinsic.module = extrinsic.extrinsic.method.section;
-        newExtrinsic.call = extrinsic.extrinsic.method.method;
-        newExtrinsic.blockHeight = extrinsic.block.block.header.number.toBigInt();
-        newExtrinsic.success = extrinsic.success;
-        newExtrinsic.isSigned = extrinsic.extrinsic.isSigned;
-        await newExtrinsic.save();
-    }
+export function handleCall(idx: string, extrinsic: SubstrateExtrinsic): Extrinsic {
+    const newExtrinsic = new Extrinsic(idx);
+    newExtrinsic.txHash = extrinsic.extrinsic.hash.toString();
+    newExtrinsic.module = extrinsic.extrinsic.method.section;
+    newExtrinsic.call = extrinsic.extrinsic.method.method;
+    newExtrinsic.blockHeight = extrinsic.block.block.header.number.toBigInt();
+    newExtrinsic.success = extrinsic.success;
+    newExtrinsic.isSigned = extrinsic.extrinsic.isSigned;
+    return newExtrinsic;
 }
-
-
