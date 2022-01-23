7852842da5658629b1d6b463d02b0799cc9dfbd6
JQQQ <jiqiang90@hotmail.com>
2022-01-24 09:39:24 +1300

    fix moonbeam chainTypes and update api version

--- moonbeam/src/chaintypes.ts
+++ moonbeam/src/chaintypes.ts
@@ -1,3 +1,3 @@
-import { typesBundlePre900 } from "moonbeam-types-bundle";
+import { typesBundleDeprecated } from "moonbeam-types-bundle";
 
-export default { typesBundle: typesBundlePre900 };
+export default { typesBundle: typesBundleDeprecated };
