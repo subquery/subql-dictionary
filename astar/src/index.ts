f5a56ea7f6efe481f32135adeb5a1d49dcb1bec3
bz888 <ziyu.ben158@gmail.com>
2022-10-07 11:06:39 +1300

    check global.atob

--- astar/src/index.ts
+++ astar/src/index.ts
@@ -1,5 +1,7 @@
 //Exports all handler functions
 import {atob} from 'abab';
-global.atob = atob;
+if (!global.atob) {
+    global.atob = atob;
+}
 export * from "./mappings/mappingHandlers";
 import "@polkadot/api-augment";
