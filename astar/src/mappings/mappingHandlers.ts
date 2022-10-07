e71692f13ccba089a5008a8e6f0d8c4909c98af8
bz888 <ziyu.ben158@gmail.com>
2022-10-07 14:19:04 +1300

    fix evmLog logic

--- astar/src/mappings/mappingHandlers.ts
+++ astar/src/mappings/mappingHandlers.ts
@@ -102,18 +102,17 @@ export function handleCall(extrinsic: SubstrateExtrinsic): Extrinsic {
 
 function handleEvmEvent(event: SubstrateEvent): EvmLogModel {
   let address;
-  let data;
+  // let data;
   let topics;
   const [log] = event.event.data as unknown as [{log:EvmLog} | EvmLog]
 
   if((log as EvmLog).address){
-    [{address, data, topics}] = log as unknown as [EvmLog];
-
+    address = (log as EvmLog).address
+    topics = (log as EvmLog).topics
   }else{
     address = (log as {log: EvmLog}).log.address;
     topics = (log as {log: EvmLog}).log.topics;
   }
-
   const evmLog = new EvmLogModel(`${event.block.block.header.number.toString()}-${event.idx}`)
   evmLog.address = address.toString()
   evmLog.blockHeight= event.block.block.header.number.toBigInt();
