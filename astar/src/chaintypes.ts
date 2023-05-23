2349f77beca039030537b7c79df4d3e5afd8f3d3
Scott Twiname <skott.twiname@gmail.com>
2023-05-23 16:16:01 +1200

    Add chaintype overrides for shiden

---
+++ astar/src/chaintypes.ts
@@ -0,0 +1,21 @@
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
+
+// structs need to be in order
+/* eslint-disable sort-keys */
+
+const definitions: OverrideBundleDefinition = {
+  types: [
+    {
+      // TODO awaiting an error at a future spec version to figure out the end range
+      minmax: [13, undefined],
+      types: {
+        DispatchErrorModule: {
+            index: 'u8',
+            error: 'u8',
+        },
+      }
+    }
+  ]
+};
+
+export default { typesBundle: { spec: { "shiden": definitions }}};
