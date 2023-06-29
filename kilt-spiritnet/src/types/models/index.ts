a174030958613770e48a3179b9c8b68217a5cda8
JQQQ <jiqiang90@hotmail.com>
2023-06-30 10:43:06 +1200

    add kilt

---
+++ kilt-spiritnet/src/types/models/index.ts
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Auto-generated , DO NOT EDIT
+
+export {SpecVersion} from "./SpecVersion"
+
+export {Event} from "./Event"
+
+export {Extrinsic} from "./Extrinsic"
+
