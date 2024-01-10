1e17acb28237bbe62167240387b7f1fffadaeb4d
Marcin Pastecki <marcin.pastecki@inndei.com>
2024-01-10 11:01:35 +0100

    Added deploy folder with files required by Jenkins pipeline

---
+++ polymesh/deploy/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -exu -o pipefail
+
+: "${AWS_REGION:=us-west-2}"
+: "${CONTAINER_REGISTRY:=201135299591.dkr.ecr.${AWS_REGION}.amazonaws.com}"
+: "${CONTAINER_TAG:=$(git rev-parse HEAD)}"
+
+docker build -f deploy/Dockerfile -t "${CONTAINER_REGISTRY}/polymesh/subquery-dictionary:${CONTAINER_TAG}" .
---
+++ polymesh/deploy/docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+
+set -xe -o pipefail
+_term() {
+	echo "Caught SIGTERM signal!"
+	kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGTERM
+
+# If NETWORK_CHAIN_ID is not set and we have an http endpoint to use, then curl the chain to fetch it (might need to retry)
+if [[ -n $NETWORK_ENDPOINT ]] && [[ -z $NETWORK_CHAIN_ID ]]
+then
+  export NETWORK_CHAIN_ID=$(curl --silent -H "Content-Type: application/json" -d '{"id":"1", "jsonrpc":"2.0", "method": "chain_getBlockHash", "params":[0]}' $NETWORK_ENDPOINT |
+  grep -o '"result":"[^"]*' |
+  grep -o '[^"]*$'
+)
+  echo "NETWORK_CHAIN_ID was set to ${NETWORK_CHAIN_ID} based on calling: $NETWORK_ENDPOINT. Production chains should explictly set NETWORK_CHAIN_ID instead"
+fi
+
+envsubst < project.template.yaml > project.yaml # Substitute environment variables in project.template.yaml
+
+# Allow configuring node memory. Default to 1.5MB, should be ~75% of available RAM
+NODE_SPACE=${MAX_OLD_SPACE_SIZE:-1536}
+
+node --max-old-space-size=$NODE_SPACE \
+	/bin/run $@ 
+
+child=$!
+wait "$child"
