e298c5e8a1e68f4507cc4c4679ac8273cd97b78f
bz888 <ziyu.ben158@gmail.com>
2024-04-18 14:43:19 +0800

    fix moonbeam ts strict

--- moonbeam-evm/src/index.ts
+++ moonbeam-evm/src/index.ts
@@ -2,7 +2,7 @@
 import {atob} from 'abab';
 
 if (!global.atob) {
-    global.atob = atob;
+    global.atob = atob as any;
 }
 export * from './mappings/mappingHandlers'
 import "@polkadot/api-augment"
