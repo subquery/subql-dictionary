7260e2a483aab6b282391b96d51ee875c8833366
JQQQ <jiqiang90@hotmail.com>
2024-05-10 12:55:39 +1200

    fix karura chaintypes.ts for 43244

--- acala/src/chaintypes.ts
+++ acala/src/chaintypes.ts
@@ -1,3 +1,31 @@
 import { typesBundleForPolkadot } from "@acala-network/type-definitions";
 
+typesBundleForPolkadot.spec.karura.types = [
+    ... typesBundleForPolkadot.spec.karura.types,
+    {
+            minmax:[0,undefined],
+            types: {
+                // @ts-ignore
+                DispatchTime: {
+                    _enum: {
+                        At: 'BlockNumber',
+                        After: 'BlockNumber'
+                    }
+                },
+                CallOf: 'Call',
+                AsOriginId: 'AuthoritysOriginId',
+                AuthoritysOriginId: {
+                    _enum: [
+                        'Root',
+                        'EaveTreasury',
+                        'CdpTreasury',
+                        'ElpTreasury',
+                        'DSWF'
+                    ]
+                },
+                DispatchErrorModule: 'DispatchErrorModuleU8'
+            },
+        }
+    ]
+
 export default { typesBundle: typesBundleForPolkadot };
