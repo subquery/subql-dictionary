9ed2b739dc1f7d964a45afd6a8aa6b858b455d7c
Marcin Pastecki <marcin.pastecki@inndei.com>
2024-11-21 11:43:35 +0100

    fix: 🐛 Get NETWORK_CHAIN_ID from the RPC node if not provided directly

--- polymesh/deploy/docker-entrypoint.sh
+++ polymesh/deploy/docker-entrypoint.sh
@@ -8,13 +8,13 @@ _term() {
 trap _term SIGTERM
 
 # If NETWORK_CHAIN_ID is not set and we have an http endpoint to use, then curl the chain to fetch it (might need to retry)
-if [[ -n $NETWORK_ENDPOINT ]] && [[ -z $NETWORK_CHAIN_ID ]]
+if [[ -n $NETWORK_HTTP_ENDPOINT ]] && [[ -z $NETWORK_CHAIN_ID ]]
 then
-  export NETWORK_CHAIN_ID=$(curl --silent -H "Content-Type: application/json" -d '{"id":"1", "jsonrpc":"2.0", "method": "chain_getBlockHash", "params":[0]}' $NETWORK_ENDPOINT |
+  export NETWORK_CHAIN_ID=$(curl --silent -H "Content-Type: application/json" -d '{"id":"1", "jsonrpc":"2.0", "method": "chain_getBlockHash", "params":[0]}' $NETWORK_HTTP_ENDPOINT |
   grep -o '"result":"[^"]*' |
   grep -o '[^"]*$'
 )
-  echo "NETWORK_CHAIN_ID was set to ${NETWORK_CHAIN_ID} based on calling: $NETWORK_ENDPOINT. Production chains should explictly set NETWORK_CHAIN_ID instead"
+  echo "NETWORK_CHAIN_ID was set to ${NETWORK_CHAIN_ID} based on calling: $NETWORK_HTTP_ENDPOINT. Production chains should explictly set NETWORK_CHAIN_ID instead"
 fi
 
 envsubst < project.template.yaml > project.yaml # Substitute environment variables in project.template.yaml
