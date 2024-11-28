f8c2b46f008104bf7e0b5798486bb196c4cc02ad
Scott Twiname <skott.twiname@gmail.com>
2024-11-28 16:02:57 +1300

    Update deps/test publish

--- publish-all.sh
+++ publish-all.sh
@@ -4,6 +4,9 @@ set -e
 for dir in */; do
     for file in "${dir}"project*.yaml; do
         echo "Publishing $file..."
+        # yarn --cwd ${dir}
+        yarn --cwd ${dir} add -D @polkadot/api@^13
+        # yarn --cwd ${dir} codegen
         npx subql publish -f "$file"
     done
 done
